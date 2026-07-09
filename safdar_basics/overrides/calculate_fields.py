e937e17fc3efe24e8987bb79bf03ba51ee4383e7
SafdarAliGit <safdar211@gmail.com>
2026-07-09 06:38:58 +0500

    updated

--- safdar_basics/overrides/calculate_fields.py
+++ safdar_basics/overrides/calculate_fields.py
@@ -1,3 +1,6 @@
+import frappe
+
+
 def before_save(doc, method=None):
     total_basic_amount = 0
     total_discount = 0
@@ -15,3 +18,7 @@ def before_save(doc, method=None):
     doc.total_basic_amount = total_basic_amount
     doc.total_discount = total_discount
     doc.total_sales_tax = total_sales_tax
+
+    # setting value for sales person
+    if doc.sales_team[0].sales_person:
+        doc.custom_sales_person = doc.sales_team[0].sales_person
